de26137d2980889ea3ce6ef5569a9da3ad86b48f
Saiteja-44 <saitejapolampally@gmail.com>
2023-09-27 17:42:34 -0700

    Intiial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './style.css';
+import WeatherDisplay from './WeatherDisplay';
+import Zipcode from './Zipcode';
+import axios from 'axios';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const apiKey = '938cc58fae1c1a679c22bbbd14c27c3b';
+
+  const handleZipcodeSubmit = async (inputZipcode) => {
+    try {
+      const zipResponse = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/zip?zip=${inputZipcode}&appid=${apiKey}`
+      );
+
+      if (zipResponse.status === 200 && zipResponse.data.lat && zipResponse.data.lon) {
+        const cityName = zipResponse.data.name;
+        const weatherResponse = await axios.get(
+          `https://api.openweathermap.org/data/3.0/onecall?lat=${zipResponse.data.lat}&lon=${zipResponse.data.lon}&appid=${apiKey}&units=imperial`
+        );
+
+        if (weatherResponse.status === 200) {
+          setWeatherData({ ...weatherResponse.data, cityName });
+        } else {
+          console.error('Failed to fetch weather data');
+        }
+      } else {
+        console.error('Failed to fetch ZIP code data');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="app">
+      <Zipcode handleZipcodeSubmit={handleZipcodeSubmit} />
+      {weatherData !== null ? (
+        <WeatherDisplay weatherData={weatherData} />
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/Zipcode.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function Zipcode({ handleZipcodeSubmit }) {
+  const [inputZipcode, setInputZipcode] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputZipcode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleZipcodeSubmit(inputZipcode);
+    setInputZipcode('');
+  };
+
+  return (
+    <div className="search">
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter ZIP code"
+          value={inputZipcode}
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSubmit(e);
+            }
+          }}
+        />
+        <i
+          className="fa fa-search search-icon"
+          onClick={handleSubmit}
+          title="Search"
+        ></i>
+      </div>
+    </div>
+  );
+}
+
+export default Zipcode;
+
+
+
+
+
