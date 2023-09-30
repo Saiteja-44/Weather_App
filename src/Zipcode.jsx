2ecd4840af2b1db2a1839e8bd6aaada8c8cc9670
Saiteja-44 <saitejapolampally@gmail.com>
2023-09-29 20:07:42 -0700

    Updated Icon

--- src/Zipcode.jsx
+++ src/Zipcode.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './style.css';
+import Icon from "./assets/weather.svg";
+
 
 function Zipcode({ handleZipcodeSubmit }) {
   const [inputZipcode, setInputZipcode] = useState('');
-  const [isCentered, setIsCentered] = useState(true);
+  const [isCentered, setIsCentered] = useState(true); // Add isCentered state
 
   const handleInputChange = (e) => {
     setInputZipcode(e.target.value);
@@ -11,13 +13,17 @@ function Zipcode({ handleZipcodeSubmit }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleZipcodeSubmit(inputZipcode);
-    setInputZipcode('');
-    setIsCentered(false); // Move to the top after submitting
+    if (inputZipcode.trim() !== '') {
+      handleZipcodeSubmit(inputZipcode);
+      setIsCentered(false); // Move the input field to the top
+    }
   };
 
   return (
     <div className={`search ${isCentered ? 'centered' : ''}`}>
+      <div>
+        <img src={Icon} alt="Landing" className="landingimg" />
+      </div>
       <div className="input-container">
         <input
           type="text"
