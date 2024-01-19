5145594532d227971b69eebf96267fa5b52b8d74
Saiteja-44 <saitejapolampally@gmail.com>
2024-01-19 12:47:00 -0800

    Updates

---
+++ precache-manifest.d857e9bf2be610e16b58c2c1f07c2044.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4843a4bf30d9c4b8bb2973efaad6723d",
+    "url": "/Weather_App/index.html"
+  },
+  {
+    "revision": "6051182d1e1c103b3b09",
+    "url": "/Weather_App/static/css/2.81804a95.chunk.css"
+  },
+  {
+    "revision": "51fc723420d35a63f1c0",
+    "url": "/Weather_App/static/css/main.a37bcfe6.chunk.css"
+  },
+  {
+    "revision": "6051182d1e1c103b3b09",
+    "url": "/Weather_App/static/js/2.e825f729.chunk.js"
+  },
+  {
+    "revision": "51fc723420d35a63f1c0",
+    "url": "/Weather_App/static/js/main.6573c905.chunk.js"
+  },
+  {
+    "revision": "46bfeaafb5c3152e5405",
+    "url": "/Weather_App/static/js/runtime~main.13f08cef.js"
+  },
+  {
+    "revision": "620fa1d8d524e5af941e7906d2e0027a",
+    "url": "/Weather_App/static/media/clear.620fa1d8.jpg"
+  },
+  {
+    "revision": "0310318eddb91fd7b71b65e812cdb61b",
+    "url": "/Weather_App/static/media/cloud.0310318e.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Weather_App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Weather_App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Weather_App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Weather_App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Weather_App/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1663f691b25b48e8e8f06bbd09a86b0c",
+    "url": "/Weather_App/static/media/landing.1663f691.jpg"
+  },
+  {
+    "revision": "10e953d915da7efc0fa2db6670fb35ac",
+    "url": "/Weather_App/static/media/other.10e953d9.jpg"
+  },
+  {
+    "revision": "f5260babc44fb248913190ebaf4c585c",
+    "url": "/Weather_App/static/media/rain.f5260bab.jpg"
+  },
+  {
+    "revision": "27b6fa00415bb7915edc05cfaf26db91",
+    "url": "/Weather_App/static/media/rainmou.27b6fa00.jpg"
+  },
+  {
+    "revision": "cd030335968682fac2f5c4ac5cc6a9de",
+    "url": "/Weather_App/static/media/snow.cd030335.jpg"
+  },
+  {
+    "revision": "4d35ad7e48bd8451a885788dd9ac516d",
+    "url": "/Weather_App/static/media/weather.4d35ad7e.svg"
+  }
+]);
